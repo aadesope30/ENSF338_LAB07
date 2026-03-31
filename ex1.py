15a2e770d8a9359bcbee4359ecc914ebf43da1d9
aadesope30 <adesope.ajayi@ucalgary.ca>
2026-03-31 11:19:48 -0600

    added Exercise 1

---
+++ ex1.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data, parent=None, left=None, right=None):
+        self.parent = parent
+        self.data = data
+        self.left = left
+        self.right = right
+
+def insert(data, root=None):
+    current = root
+    parent = None
+
+    while current is not None:
+        parent = current
+        if data <= current.data:
+            current = current.left
+        else:
+            current = current.right
+
+    newnode = Node(data, parent)    
+    if root is None:
+        root = newnode
+    elif data <= parent.data:
+        parent.left = newnode
+    else:
+        parent.right = newnode
+
+    return newnode
+
+def search(data, root):
+    current = root
+    while current is not None:
+        if data == current.data:
+            return current
+        elif data <= current.data:
+            current = current.left
+        else:
+            current = current.right
+    return None
